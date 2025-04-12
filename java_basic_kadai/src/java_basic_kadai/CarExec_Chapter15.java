abc9231bce2d19b1427b8d8ebdad5b9b5a64685a
Masaki_E <masakawa1995@gmail.com>
2025-04-12 22:15:33 +0900

    課題021：辞書プログラム追加

---
+++ java_basic_kadai/src/java_basic_kadai/CarExec_Chapter15.java
@@ -0,0 +1,13 @@
+package java_basic_kadai;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Car_Chapter15 car = new Car_Chapter15(); // 車のインスタンスを生成
+		car.gearChange(3);                       // ギアを3に変更
+		car.run();                               // スピードを表示
+
+	}
+
+}
